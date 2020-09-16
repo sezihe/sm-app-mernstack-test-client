cf15b3974f96149072ec5411d2eefec3b03272f3
EZIHE SOUZOCHUKWU DANIEL <ezihesouzo@yahoo.com>
2020-09-17 00:09:03 +0100

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super();
+    // set state
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Would be back! 💙
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
